99c50c995d3d0e508586781800f39d7e65f9bf40
yannuser <hienpollo@gmail.com>
2026-05-02 19:01:30 -0400

    initial commit

--- backend/models/User.py
+++ backend/models/User.py
@@ -1,4 +1,23 @@
-from mongoengine.document import Document
+from mongoengine import Document, StringField, EmailField, DateField
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import re
 
+def check_birthdate(given_date):
+    now = datetime.now()
+    if given_date > now - relativedelta(years= 10):
+        raise ValueError("Date is wrong.")
+
+def check_passwork(given_pwd):
+    pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+
+    if not re.findall(pattern, given_pwd):
+        raise ValueError("Password's too short.")
 
 class User(Document):
+    email = EmailField(required=True, unique=True)
+    first_name = StringField(required=True, max_length=50)
+    last_name = StringField(required=True, max_length=50)
+    birthdate = DateField(required=True, validation=check_birthdate)
+    username = StringField(rquired=True, unique=True)
+    password = StringField(required=True, validate=check_passwork)
